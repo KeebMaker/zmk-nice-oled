32714943679a4b8a7de3825dcb94fe571f08094d
KeebMaker <admin@keebmaker.com>
2026-06-20 01:59:05 -0600

    Add static KM medal snapshot option for peripheral display
    Reuses the first frame of the existing rotating KM medal animation
    (full round, face-on view) as a static image, selectable via
    CONFIG_NICE_OLED_WIDGET_STATIC_IMAGE_PERIPHERAL_KM. Lets boards keep
    the medal/stars artwork without the continuous LVGL animation tick
    that was driving frequent I2C flushes on the peripheral half.

--- boards/shields/nice_oled/widgets/animation.c
+++ boards/shields/nice_oled/widgets/animation.c
@@ -171,6 +171,10 @@ LV_IMG_DECLARE(vim);
 LV_IMG_DECLARE(vip_marcos);
 #define FIXED_IMAGE_VIP_MARCOS &vip_marcos
 
+#elif IS_ENABLED(CONFIG_NICE_OLED_WIDGET_STATIC_IMAGE_PERIPHERAL_KM)
+#include "../assets/km_medal.h"
+#define FIXED_IMAGE_KM &km_medal_00
+
 #else
 
 LV_IMG_DECLARE(vim);
@@ -211,6 +215,8 @@ void draw_animation(lv_obj_t *canvas, struct zmk_widget_screen *widget) {
     lv_img_set_src(art, FIXED_IMAGE_VIM);
 #elif IS_ENABLED(CONFIG_NICE_OLED_WIDGET_STATIC_IMAGE_PERIPHERAL_VIP_MARCOS)
     lv_img_set_src(art, FIXED_IMAGE_VIP_MARCOS);
+#elif IS_ENABLED(CONFIG_NICE_OLED_WIDGET_STATIC_IMAGE_PERIPHERAL_KM)
+    lv_img_set_src(art, FIXED_IMAGE_KM);
 #else
     int length = sizeof(crystal_imgs) / sizeof(crystal_imgs[0]);
     srand(k_uptime_get_32());
